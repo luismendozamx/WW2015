0b8941a2ff83f56b94128eb072050e9d5228842f
Luis Mendoza <luis@mendoza.io>
2015-02-10 21:20:49 -0600

    Added images and initial landing page structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, :last_name, presence: true
+
 
   def full_name
     first_name + " " + last_name
