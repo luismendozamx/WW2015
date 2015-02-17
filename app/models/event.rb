0ba85881862cdee845b20e5d03e3d1b6f361be58
Luis Mendoza <luis@mendoza.io>
2015-02-17 17:14:33 -0600

    Added seeds and Event validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :speaker
   belongs_to :location
 
+  validates :name, :speaker_id, :location_id, presence: true
+
   # Friendly Id
   extend FriendlyId
   friendly_id :name, use: :slugged
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(first_name: "Luis", last_name: "Mendoza", email: "luis@mendoza.io", password: "luismendoza")
+User.create!(first_name: "Enrique", last_name: "Mendoza", email: "luis@luismendoza.mx", password: "luismendoza")
+
+Speaker.create(first_name: "Paola", last_name: "Reyner")
+Speaker.create(first_name: "Sofía", last_name: "García")
+Speaker.create(first_name: "Eva", last_name: "Pérez")
+
+Location.create(name: "Auditorio")
+Location.create(name: "Sala de Conferencias")
+Location.create(name: "Salon")
+
+Event.create(name: "Evento 1", speaker_id: 1, location_id: 1)
+Event.create(name: "Evento 2", speaker_id: 2, location_id: 2)
+Event.create(name: "Evento 3", speaker_id: 3, location_id: 3)
