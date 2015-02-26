72ec7b1b027889afe6bd64f1852ae95e8fae49a3
Luis Mendoza <luis@mendoza.io>
2015-02-26 12:10:08 -0600

    Removed migration

--- db/migrate/20150226175448_add_limit_to_events.rb
+++
@@ -1,8 +0,0 @@
-class AddLimitToEvents < ActiveRecord::Migration
-  def change
-    add_column :events, :limit, :integer
-    add_column :events, :limit_banamex, :integer
-    add_column :workshops, :limit, :integer
-    add_column :workshops, :limit_banamex, :integer
-  end
-end
