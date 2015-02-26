9eaa73fcb0f504ed8b178ae1e7f3d6345317f5bf
Luis Mendoza <luis@mendoza.io>
2015-02-26 15:42:12 -0600

    Added button disabled for full event

--- app/validators/folio_validator.rb
+++ app/validators/folio_validator.rb
@@ -1,6 +1,6 @@
 class FolioValidator < ActiveModel::Validator
   def validate(record)
-    unless (1..4510).include?(record.folio) && User.where(folio: record.folio).count < 2
+    unless (1..4503).include?(record.folio) && User.where(folio: record.folio).count < 2
       record.errors[:folio] << 'Folio Invalido'
     end
   end
